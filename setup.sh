e6290b394211579fcbd49ffe52f19abfa725b111
BlockListed <44610569+BlockListed@users.noreply.github.com>
2022-07-02 18:02:21 +0200

    auto move cronjob and remind to enable NTP
    The raspberrypi does not have a realtime-clock, so it's important to
    syncronise NTP upon startup and runtime, to make sure the timestamps
    remain accurate.

--- setup.sh
+++ setup.sh
@@ -10,3 +10,7 @@ sudo pip3 install --upgrade setuptools
 sudo apt-get install i2c-tools libgpiod-dev -y
 
 pip3 install -r requirements.txt
+
+cp cronjob/raspberrytemperature /etc/cron.d/raspberrytemperature
+
+echo "Remember to setup NTP for accurate Timestamps."
